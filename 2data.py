68dec885bedfac3f79e2b6850b3862cd09d4e1e4
sumyth010 <sumyth01@gmail.com>
2025-08-06 19:56:05 +0545

    data

---
+++ 2data.py
@@ -0,0 +1,48 @@
+
+# String(str) 
+x = "Hello World"
+
+# Intreger(int)
+x = 20 
+
+# Foat(float)
+x = 20.5
+
+# Complex(complex)
+x = 1j
+
+# List 
+x = ["apple", "banana", "Cherry"]
+
+# Tuple
+x = ("apple", "banana", "cherry")
+
+#  range
+x = range(6)
+
+# dictionary(dict)
+x = {"name": "john", "age": 36}
+
+# Set 
+x = {"apple", "banana", "cherry"}
+
+# Frozenset
+x = frozenset({
+    "apple", "banana", "cherry"
+})
+
+# bool 
+x= True
+
+# Byte 
+x = b"Hello"
+
+# bytearray
+x = bytearray(5)
+
+# memoryview
+x = memoryview(bytes(5))
+
+# NoneType 
+x = None
+
