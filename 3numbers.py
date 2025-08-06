d6c05c8d781d0c38478ff4b5fec0ea0b91496dd9
sumyth010 <sumyth01@gmail.com>
2025-08-06 21:01:08 +0545

    Number

---
+++ 3numbers.py
@@ -0,0 +1,63 @@
+
+
+# Numbers
+
+# int
+x = 1
+y = 234564323456
+z = -1234567
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+
+# Float
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+
+# Complex
+x = 3 + 5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+
+
+# Type Conversion
+x = 1
+y = 2.8
+z = 1j
+
+# Convert from int to float
+a = float(x)
+
+# Convert from float to int
+b = int(y)
+
+# Convert form int to complex
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+
+print(type(a))
+print(type(b))
+print(type(c))
