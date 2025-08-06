3ed0babd228998354603eb5b8d23f9ea2d3cb290
sumyth010 <sumyth01@gmail.com>
2025-08-06 19:31:21 +0545

    Variables

---
+++ 1variables.py
@@ -0,0 +1,115 @@
+
+
+# Varaiables are containers for storing data vales
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+# Casting 
+x = str(3)
+y = int(3)
+z = float(3)
+
+# Get the type of a variable
+
+x = 5
+y = "John"
+
+print(type(x))
+print(type(y))
+
+
+
+
+
+
+
+
+
+
+
+
+# Varabiles Names 
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+
+# Many values to multiple variables
+x , y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+# One Value to multiple Variables 
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+# Unpack a collection
+fruits = ["apple", "banana", "Cherry"]
+x,y,z = fruits
+print(x)
+print(y)
+print(z)
+
+
+
+
+
+
+
+
+
+
+
+# Output variables
+x = "Python is Awesome"
+print(x)
+
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+print( x + y + z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Global Varaibles
+x = "awesome"
+
+def myfunc():
+    x = "Fantastic"
+    print("Python is " + x)
+
+myfunc()
+
+print("python si " + x)
+
+
+
+
